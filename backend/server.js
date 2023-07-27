24044469e4015d0a1b434f1f56179f88f8b03437
Aayush <aayushchaudhari1@gmail.com>
2023-07-28 05:07:04 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const express =require('express')
+const mongoose= require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes= require('./routes/user')
+
+//express app
+const app=express()
+
+//middleware
+app.use(express.json())
+
+app.use((req,res,next)=> {
+    console.log(res.path, req.method)
+    next()
+})
+
+//routes
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
+
+//connect to db
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+    console.log('db &', process.env.PORT)
+    })  
+ })
+.catch((error)=>{
+    console.log(error)
+})
+
